30f11a1022aa3b93eced7b29e102bfcd297dfe81
jinwood <jinw@protonmail.com>
2020-07-18 17:12:14 +0100

    Styles

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(
+    getByText(/How many trees has Ecologi planted in the past/i)
+  ).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import { TreeCount } from "./features/tree-count/TreeCount";
 import "./App.css";
 
